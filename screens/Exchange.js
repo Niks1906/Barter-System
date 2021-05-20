794225022adfacf0cab4776200aa3c1a5c2b2662
Niks1906 <66248757+Niks1906@users.noreply.github.com>
2021-05-20 07:53:07 +0530

    Add files via upload

--- screens/Exchange.js
+++ screens/Exchange.js
@@ -15,7 +15,9 @@ export default class Exchange extends Component {
       requestedItemName:"",
       exchangeId:"",
       itemStatus:"",
-      docId: ""
+      docId: "",
+      itemValue:"",
+      currencyCode:""
 
     }
   }
@@ -25,14 +27,17 @@ export default class Exchange extends Component {
   }
 
   addItem= async(itemName, description)=>{
+
     var userName = this.state.userName
-    exchangeId = this.createUniqueId()
+    var exchangeId = this.createUniqueId()
+    console.log("im called",exchangeId);
     db.collection("exchange_requests").add({
       "username"    : userName,
       "item_name"   : itemName,
       "description" : description,
       "exchangeId"  : exchangeId,
       "item_status" : "requested",
+      "item_value"  : this.state.itemValue,
         "date"       : firebase.firestore.FieldValue.serverTimestamp()
 
      })
@@ -50,7 +55,8 @@ export default class Exchange extends Component {
 
      this.setState({
        itemName : '',
-       description :''
+       description :'',
+       itemValue : ""
      })
 
 
@@ -82,7 +88,8 @@ export default class Exchange extends Component {
       querySnapshot.forEach(doc => {
         this.setState({
           IsExchangeRequestActive:doc.data().IsExchangeRequestActive,
-          userDocId : doc.id
+          userDocId : doc.id,
+          currencyCode: doc.data().currency_code
         })
       })
     })
@@ -100,6 +107,7 @@ export default class Exchange extends Component {
             exchangeId : doc.data().exchangeId,
             requestedItemName: doc.data().item_name,
             itemStatus:doc.data().item_status,
+            itemValue : doc.data().item_value,
             docId     : doc.id
           })
         }
@@ -107,14 +115,29 @@ export default class Exchange extends Component {
   })
 }
 
-  componentDidMount(){
-    this.getExchangeRequest()
-    this.getIsExchangeRequestActive()
-
+getData(){
+  fetch("http://data.fixer.io/api/latest?access_key=1f7dd48123a05ae588283b5e13fae944&format=1")
+  .then(response=>{
+    return response.json();
+  }).then(responseData =>{
+    var currencyCode = this.state.currencyCode
+    var currency = responseData.rates.INR
+    var value =  69 / currency
+    console.log(value);
+  })
   }
 
 
-  receivedItem=(bookName)=>{
+
+
+  componentDidMount(){
+    this.getExchangeRequest()
+    this.getIsExchangeRequestActive()
+    this.getData()
+  }
+
+
+  receivedItem=(itemName)=>{
     var userId = this.state.userName
     var exchangeId = this.state.exchangeId
     db.collection('received_items').add({
@@ -184,6 +207,11 @@ export default class Exchange extends Component {
          <Text>{this.state.requestedItemName}</Text>
          </View>
          <View style={{borderColor:"orange",borderWidth:2,justifyContent:'center',alignItems:'center',padding:10,margin:10}}>
+         <Text> Item Value </Text>
+
+         <Text>{this.state.itemValue}</Text>
+         </View>
+         <View style={{borderColor:"orange",borderWidth:2,justifyContent:'center',alignItems:'center',padding:10,margin:10}}>
          <Text> Item Status </Text>
 
          <Text>{this.state.itemStatus}</Text>
@@ -229,6 +257,17 @@ export default class Exchange extends Component {
             }}
             value={this.state.description}
 
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder ={"Item Value"}
+            maxLength ={8}
+            onChangeText={(text)=>{
+              this.setState({
+                itemValue: text
+              })
+            }}
+            value={this.state.itemValue}
           />
           <TouchableOpacity
             style={[styles.button,{marginTop:10}]}
